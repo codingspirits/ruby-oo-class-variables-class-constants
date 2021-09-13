e77afd9b8ca233ad9a0fb44ae3c9c8f4182656ae
vincci-cheng-project-development-page <vincci.cheng.github@gmail.com>
2021-09-13 18:11:15 +1000

    Completed Assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,25 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = []
 
   def initialize(title)
     @title = title
+    #@GENRES = []
   end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
 
+  def genre=(genre)
+    @genre = genre
+    GENRES.push(genre)
+  end
 end
+
+book1 = Book.new("Gone with the Wind")
+book1.genre="Fiction"
+p book1
+p book1.genre
